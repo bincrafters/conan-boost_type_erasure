b17ab9f7913622d3314c53980fd138b1d07e6d84
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-05 12:06:50 -0400

    add build_requires and settings to conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,16 +3,34 @@ from conans import ConanFile, tools, os
 class BoostType_ErasureConan(ConanFile):
     name = "Boost.Type_Erasure"
     version = "1.64.0"
-    generators = "txt"
+    generators = "txt" 
+    settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/type_erasure"
-    description = "For a description of this library, please visit http://boost.org/type_erasure "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "type_erasure"
+    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
+    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Fusion/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
+                      "Boost.Typeof/1.64.0@bincrafters/testing"
+
+                      #assert1 config0 core2 fusion5 iterator5 mpl5 preprocessor0 smart_ptr4 throw_exception2 type_traits3 typeof5
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
-        self.copy(pattern="*", dst="", src=include_dir)
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
+        self.copy(pattern="*", dst="include", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
