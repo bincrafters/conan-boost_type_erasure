30b208456c8284eb9f94087f8dc5241507fc0c2c
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:22 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,8 @@ class BoostType_ErasureConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
-    url = "https://github.com/boostorg/type_erasure"
+    url = "https://github.com/bincrafters/conan-boost-type_erasure"
+    source_url = "https://github.com/boostorg/type_erasure"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "type_erasure"
@@ -27,7 +28,7 @@ class BoostType_ErasureConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def build(self):
         boost_build = self.deps_cpp_info["Boost.Build"]
@@ -52,4 +53,4 @@ class BoostType_ErasureConan(ConanFile):
         self.copy(pattern="*", dst="lib", src=lib_dir)
 
     def package_info(self):
-        self.cpp_info.libs = ["boost_%s"%(self.lib_short_name)]
+        self.cpp_info.libs = self.collect_libs()
