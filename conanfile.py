c2bc53d2172833492c7cea18ed32aa52211b8528
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:15 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostType_ErasureConan(base.BoostBaseConan):
     name = "boost_type_erasure"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_type_erasure"
     lib_short_names = ["type_erasure"]
     options = {"shared": [True, False]}
